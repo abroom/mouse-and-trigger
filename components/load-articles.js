7b30ed33004f43735c6ddcf3c2577e85634272f1
Alexander Broom <alex.broom@gmail.com>
2014-10-05 20:33:20 -0600

    fixed navButton functionality

--- components/load-articles.js
+++ components/load-articles.js
@@ -1,38 +1,63 @@
 // Loads articles for display on index
+
+var articlesPerPage = 10;
+
 function loadArticles(category) {
 	loadDirectory(category);
 }
 
 
 function loadDirectory(category) {
-	$.get (
-		"articles/directory.txt",
-		function(directory) {
-			navDirectory(directory, category)
-		},
-		'text'
-	);
+	var order = getOrder();
+	if (order == "ascending") {
+		$.get (
+			"articles/directory.txt",
+			function(directory) {
+				navDirectoryAscending(directory, category)
+			},
+			'text'
+		);
+	} else {
+		$.get (
+			"articles/directory.txt",
+			function(directory) {
+				navDirectoryDescending(directory, category)
+			},
+			'text'
+		);
+	}
 }
 
-function navDirectory(directory, category) {
+function navDirectoryAscending(directory, category) {
 	var dirItems = directory.split("\n");
-	var articlesPerPage = 10;
 	var index = getIndex();
+	var startingIndex = index;
 
 	if (category) {
 		var i = 0;
-		while(i < articlesPerPage && index < dirItems.length) {
-			var article = dirItems[index].split(" ");
-			if (category == article[2]) {
-				loadArticle(article[0],article[1]);
-				i++;
+		if (category == "other") {
+			while(i < articlesPerPage && index < dirItems.length) {
+				var article = dirItems[index].split(" ");
+				if ("videoGames" != article[2]) {
+					loadArticleAscending(article[0],article[1]);
+					i++;
+				}
+				index++;
 			}
-			index++;
+		} else {
+			while(i < articlesPerPage && index < dirItems.length) {
+				var article = dirItems[index].split(" ");
+				if (category == article[2]) {
+					loadArticleAscending(article[0],article[1]);
+					i++;
+				}
+				index++;
+			}
 		}
 	} else {
 		for (var i = 0; i < articlesPerPage && index < dirItems.length; i++) {
 			var article = dirItems[index].split(" ");
-			loadArticle(article[0],article[1]);
+			loadArticleAscending(article[0],article[1]);
 			index++;
 		}
 	}
@@ -43,26 +68,98 @@ function navDirectory(directory, category) {
 
 	var navButtons = '<ul>';
 	if (index < dirItems.length) {
-		navButtons += '<li><a class="button" href="'+category+'.html?index='+index+'">Older Articles</a></li>';
+		navButtons += '<li><a class="button" href="'+category+'.html'
+				   +'?index='+index+'">Older Articles</a></li>';
 	}
 	if (index > articlesPerPage) {
-		navButtons += '<li><a class="button" href="'+category+'.html?index='+(index-2*articlesPerPage)+'">Newer Articles</a></li>'
+		navButtons += '<li><a class="button" href="'+category+'.html'
+				   +'?index='+(startingIndex-1)
+				   +'&amp;order=descending">Newer Articles</a></li>';
 	}
 	navButtons += '</ul>';
 
 	$("#navButtons").append(navButtons);
 }
-
-function loadArticle(date, fileName) {
+function loadArticleAscending(date, fileName) {
 	$.get (
 		'articles/'+date+'/'+fileName+'.html',
 		function(article) {
-			writeArticle(article, date, fileName);
+			writeArticleAscending(article, date, fileName);
 		},
 		'html'
 	);
 }
+function writeArticleAscending(article, date, fileName) {
+	$("#articles").append(writeArticle(article, date, fileName));
+}
 
+function navDirectoryDescending(directory, category) {
+	var dirItems = directory.split("\n");
+	var index = getIndex();
+	var startingIndex = index;
+
+	if (category) {
+		var i = 0;
+		if (category == "other") {
+			while(i < articlesPerPage && index >= 0) {
+				var article = dirItems[index].split(" ");
+				if ("videoGames" != article[2]) {
+					loadArticleDescending(article[0],article[1]);
+					i++;
+				}
+				index--;
+			}
+		} else {
+			while(i < articlesPerPage && index >= 0) {
+				var article = dirItems[index].split(" ");
+				if (category == article[2]) {
+					loadArticleDescending(article[0],article[1]);
+					i++;
+				}
+				index--;
+			}
+		}
+	} else {
+		for (var i = 0; i < articlesPerPage && index >= 0; i++) {
+			var article = dirItems[index].split(" ");
+			loadArticleDescending(article[0],article[1]);
+			index--;
+		}
+	}
+
+	if (!category) {
+		category = "index";
+	}
+
+	var navButtons = '<ul>';
+	if (index < dirItems.length) {
+		navButtons += '<li><a class="button" href="'+category+'.html'
+				   +'?index='+(startingIndex+1)+'">Older Articles</a></li>';
+	}
+	if (index > articlesPerPage) {
+		navButtons += '<li><a class="button" href="'+category+'.html'
+				   +'?index='+index
+				   +'&amp;order=descending">Newer Articles</a></li>';
+	}
+	navButtons += '</ul>';
+
+	$("#navButtons").append(navButtons);
+}
+function loadArticleDescending(date, fileName) {
+	$.get (
+		'articles/'+date+'/'+fileName+'.html',
+		function(article) {
+			writeArticleDescending(article, date, fileName);
+		},
+		'html'
+	);
+}
+function writeArticleDescending(article, date, fileName) {
+	$("#articles").prepend(writeArticle(article, date, fileName));
+}
+
+
+// writes article summary
 function writeArticle(article, date, fileName) {
 	var link = 'href="article.html?date='+date+'&amp;article='+fileName+'"';
 	var title = $(article).find('#article-title').html();
@@ -83,10 +180,10 @@ function writeArticle(article, date, fileName) {
 			   + '<p class="blurb">'+blurb+'</p>'
 			   + '<a class="continue-reading" '+link+'>Continue Reading ...</a>'
 			   + '</div>';
-	$("#articles").append(output);
+	return output;
 }
 
-// returns page number
+// returns index
 function getIndex() {
 	var query = window.location.search.substring(1);
 	var vars = query.split("&");
@@ -102,4 +199,18 @@ function getIndex() {
 	return parseInt(index);
 }
 
+// returns order of output
+function getOrder() {
+	var query = window.location.search.substring(1);
+	var vars = query.split("&");
+	var order = "ascending";
 
+	for (var i=0; i<vars.length; i++) {
+		var pair = vars[i].split("=");
+		if(pair[0] == "order") {
+			order = pair[1];
+		}
+	}
+
+	return order;
+}
